85d5833a5af06ee6248363b1ab31d08ac04fe437
Le Nguyen Phuong Thai <thailnp133@gmail.com>
2023-03-06 22:08:22 +0700

    danh sách người dùng

--- src/pages/admin/user/ListUserPage.jsx
+++ src/pages/admin/user/ListUserPage.jsx
@@ -1,11 +1,220 @@
-import React from 'react';
-
-const ListUserPage = () => {
+import React from "react";
+import { SearchOutlined } from "@ant-design/icons";
+import { Avatar, Button, Input, Space, Table } from "antd";
+import { useRef, useState } from "react";
+import Highlighter from "react-highlight-words";
+const data = [
+  {
+    key: "1",
+    taikhoan: "Thaile13032001",
+    hoten: "Lê nguyễn phương thái",
+    ngaysinh: "1990-01-01",
+    email: "abs@gmail.com",
+    SDT: "0789130321",
+    diachi: "New York No. 1 Lake Park",
+    quyen: "Người dùng",
+    ngaytao: "20/02/2022",
+    ngaychinhsua: "20/02/2022",
+  },
+];
+export default function ListUserPage() {
+  const [searchText, setSearchText] = useState("");
+  const [searchedColumn, setSearchedColumn] = useState("");
+  const searchInput = useRef(null);
+  const handleSearch = (selectedKeys, confirm, dataIndex) => {
+    confirm();
+    setSearchText(selectedKeys[0]);
+    setSearchedColumn(dataIndex);
+  };
+  const handleReset = (clearFilters) => {
+    clearFilters();
+    setSearchText("");
+  };
+  const handleEdit = (value) => {
+    console.log("value:", value);
+    
+  }
+  const handleDelete = (value) => {
+    console.log("value:", value);
+    
+  }
+  const getColumnSearchProps = (dataIndex) => ({
+    filterDropdown: ({
+      setSelectedKeys,
+      selectedKeys,
+      confirm,
+      clearFilters,
+      close,
+    }) => (
+      <div
+        style={{
+          padding: 8,
+        }}
+        onKeyDown={(e) => e.stopPropagation()}
+      >
+        <Input
+          ref={searchInput}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={(e) =>
+            setSelectedKeys(e.target.value ? [e.target.value] : [])
+          }
+          onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+          style={{
+            marginBottom: 8,
+            display: "block",
+          }}
+        />
+        <Space>
+          <Button
+            type="primary"
+            onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+            icon={<SearchOutlined />}
+            size="small"
+            style={{
+              width: 90,
+            }}
+          >
+            Search
+          </Button>
+          <Button
+            onClick={() => clearFilters && handleReset(clearFilters)}
+            size="small"
+            style={{
+              width: 90,
+            }}
+          >
+            Reset
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              confirm({
+                closeDropdown: false,
+              });
+              setSearchText(selectedKeys[0]);
+              setSearchedColumn(dataIndex);
+            }}
+          >
+            Filter
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              close();
+            }}
+          >
+            close
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: (filtered) => (
+      <SearchOutlined
+        style={{
+          color: filtered ? "#1890ff" : undefined,
+        }}
+      />
+    ),
+    onFilter: (value, record) =>
+      record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
+    onFilterDropdownOpenChange: (visible) => {
+      if (visible) {
+        setTimeout(() => searchInput.current?.select(), 100);
+      }
+    },
+    render: (text) =>
+      searchedColumn === dataIndex ? (
+        <Highlighter
+          highlightStyle={{
+            backgroundColor: "#ffc069",
+            padding: 0,
+          }}
+          searchWords={[searchText]}
+          autoEscape
+          textToHighlight={text ? text.toString() : ""}
+        />
+      ) : (
+        text
+      ),
+  });
+  const columns = [
+    {
+      title: "username",
+      dataIndex: "taikhoan",
+      key: "taikhoan",
+      ...getColumnSearchProps("taikhoan"),
+    },
+    {
+      title: "Họ và tên",
+      dataIndex: "hoten",
+      key: "hoten",
+      ...getColumnSearchProps("hoten"),
+    },
+    {
+      title: "Ngày sinh",
+      dataIndex: "ngaysinh",
+      key: "ngaysinh",
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+      key: "email",
+      ...getColumnSearchProps("email"),
+    },
+    {
+      title: "Số điện thoại",
+      dataIndex: "SDT",
+      key: "SDT",
+      ...getColumnSearchProps("SDT"),
+    },
+    {
+      title: "Ngày tạo",
+      dataIndex: "ngaytao",
+      key: "ngaytao",
+    },
+    {
+      title: "Ngày chỉnh sửa",
+      dataIndex: "ngaychinhsua",
+      key: "ngaychinhsua",
+    },
+    {
+      title: "Quyền",
+      dataIndex: "quyen",
+      key: "quyen",
+    },
+    {
+      title: "Địa chỉ",
+      dataIndex: "diachi",
+      key: "diachi",
+      ...getColumnSearchProps("diachi"),
+      sorter: (a, b) => a.address.length - b.address.length,
+      sortDirections: ["descend", "ascend"],
+    },
+    {
+      title: "Hành động",
+      key: "hanhdong",
+      render: (text, record) => (
+        <Space size="middle">
+          <Button type="primary" onClick={() => handleEdit(record)}>
+            Edit
+          </Button>
+          <Button type="danger" onClick={() => handleDelete(record)}>
+            Delete
+          </Button>
+        </Space>
+      ),
+    },
+  ];
   return (
-    <div>
-      <h1>Danh sách người dùng</h1>
+    <div className="usertable bg-adminPrimary flex-grow rounded-lg select-none overflow-auto">
+      <Table
+        columns={columns}
+        dataSource={data}
+        scroll={{ x: "max-content" }}
+      />
     </div>
   );
-};
-
-export default ListUserPage;
+}
