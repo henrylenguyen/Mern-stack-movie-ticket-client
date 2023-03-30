111d574edfa8eaeb327970258a570a73622551d9
TYRA <maitien2507@gmail.com>
2023-03-30 15:28:21 +0700

    các cấu hình của dự án

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,37 @@
-import Footer from "layouts/Footer";
-import { Route, Routes } from "react-router-dom";
-import Navbar from "./components/layout/Navbar";
-import NotFoundPage from "./pages/NotFoundPage";
-import routes from "./routes/routes";
+import PageNotFound from 'pages/NotFoundPage';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { Route, Routes, useNavigate } from 'react-router-dom';
+import routes from 'routes/routes';
+import { fetchProfile } from 'thunks/userThunk';
+
+import { fetchLayThongTinTaiKhoan } from 'thunks/admin/userThunks';
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchProfile());
+    dispatch(fetchLayThongTinTaiKhoan());
+  }, []);
   return (
     <>
       <Routes>
-        {/* Thay vì để Header chỗ này nó sẽ lun xuất hiện */}
-        <Route path="/" element={<Navbar />}>
-          {/* Đưa nó vào route để quản lí  - 1 route có thể chứa nhiều route con - Giống như 1 component chứa nhiều component con */}
-          {routes?.map(({ path, component: Component }, index) => (
-            <Route key={index} path={path} element={<Component />}></Route>
-          ))}
-        </Route>
-
-        {/* Đây là not found */}
-        <Route path="*" element={<NotFoundPage />}></Route>
+        {routes?.map(({ path, component: Component, children }, idx) => {
+          return (
+            <Route key={idx} path={path} element={<Component />}>
+              {children?.map(({ path, component: Component, index }, idx) => (
+                <Route
+                  key={idx}
+                  index={index}
+                  path={path}
+                  element={<Component />}
+                />
+              ))}
+            </Route>
+          );
+        })}
+        <Route path='*' element={<PageNotFound />}></Route>
       </Routes>
-      <Footer></Footer>
     </>
   );
 }
